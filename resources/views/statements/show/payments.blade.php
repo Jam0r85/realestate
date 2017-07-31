4bade38204b2742458ced67352f661b1dd6274ba
= <=>
2017-07-31 11:41:09 +0100

    Statement tweaks

--- resources/views/statements/show/payments.blade.php
+++ resources/views/statements/show/payments.blade.php
@@ -44,20 +44,26 @@
 		@endcomponent
 
 		@if ($statement->paid_at || $statement->sent_at)
-
 			@component('partials.notifications.primary')
 				This statement has been paid in full. You cannot re-generate the payments.
 			@endcomponent
-
 		@else
+
+			<div class="content">
+
+				<p>
+					Create the required payments to the landlord, business and contractors by clicking the button below.
+				</p>
+
+			</div>
+
+			<hr />
 
 			<form role="form" method="POST" action="{{ route('statements.create-payments', $statement->id) }}">
 				{{ csrf_field() }}
-
 				@component('partials.forms.buttons.primary')
 					{{ count($statement->payments) ? 'Re-Generate' : 'Generate' }} Payments
 				@endcomponent
-
 			</form>
 
 		@endif
