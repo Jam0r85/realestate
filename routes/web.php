4d0df2d59402791212ec7a555b1b201531d6b272
= <=>
2017-08-05 20:19:46 +0100

    added dashboard template

--- routes/web.php
+++ routes/web.php
@@ -10,14 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'DashboardController@index');
 
 Route::prefix('calendars')->group(function () {
 	Route::get('/', 'CalendarController@index')->name('calendars.index');
