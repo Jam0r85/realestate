69b4cb157fe2ab13f9559b09b7293e34cad9554e
= <=>
2017-08-08 17:44:44 +0100

    updated property tenancies panel

--- resources/views/properties/show/layout.blade.php
+++ resources/views/properties/show/layout.blade.php
@@ -102,12 +102,16 @@
 									<thead>
 										<th>Name</th>
 										<th>Rent</th>
+										<th>Started</th>
+										<th>Status</th>
 									</thead>
 									<tbody>
 										@foreach ($property->tenancies as $tenancy)
 											<tr>
-												<td>{{ $tenancy->name }}</td>
+												<td><a href="{{ route('tenancies.show', $tenancy->id) }}">{{ $tenancy->name }}</a></td>
 												<td>{{ currency($tenancy->rent_amount) }}</td>
+												<td>{{ date_formatted($tenancy->started_at) }}</td>
+												<td>{{ $tenancy->vacated_on ? 'Vacated' : 'Active' }}</td>
 											</tr>
 										@endforeach
 									</tbody>
