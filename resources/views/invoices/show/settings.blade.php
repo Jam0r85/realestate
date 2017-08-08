e2fe3bd5c9fe9100456235ac4c47e8d68a1be8f3
= <=>
2017-08-08 12:58:06 +0100

    updated invoices

--- resources/views/invoices/show/settings.blade.php
+++ resources/views/invoices/show/settings.blade.php
@@ -8,71 +8,61 @@
 			Settings
 		@endcomponent
 
-		@if ($invoice->paid_at || $invoice->trashed())
+		<form role="form" method="POST" action="{{ route('invoices.update', $invoice->id) }}">
+			{{ csrf_field() }}
+			{{ method_field('PUT') }}
 
-			@component('partials.notifications.primary')
-				You cannot update the settings for this invoice as it's been paid or archived.
+			<div class="field">
+				<label class="label" for="created_at">Date Created</label>
+				<p class="control">
+					<input type="date" class="input" name="created_at" value="{{ $invoice->created_at->format('Y-m-d') }}" />
+				</p>
+			</div>
+
+			<div class="field">
+				<label class="label" for="due_at">Date Due</label>
+				<p class="control">
+					<input type="date" class="input" name="due_at" value="{{ $invoice->due_at ? $invoice->due_at->format('Y-m-d') : null }}" />
+				</p>
+			</div>
+
+			<div class="field">
+				<label class="label" for="number">Number</label>
+				<p class="control">
+					<input type="number" class="input" name="number" value="{{ $invoice->number }}" />
+				</p>
+			</div>
+
+			<div class="field">
+				<label class="label" for="user_id">Users</label>
+				<p class="control ix-expanded">
+					<select name="user_id[]" class="select2" multiple>
+						@foreach (users() as $user)
+							<option @if ($invoice->users->contains($user->id)) selected @endif value="{{ $user->id }}">{{ $user->name }}</option>
+						@endforeach
+					</select>
+				</p>
+			</div>
+
+			<div class="field">
+				<label class="label" for="recipient">Recipient</label>
+				<p class="control">
+					<textarea name="recipient" class="textarea">{{ $invoice->recipient }}</textarea>
+				</p>
+			</div>
+
+			<div class="field">
+				<label class="label" for="terms">Terms</label>
+				<p class="control">
+					<textarea name="terms" class="textarea">{{ $invoice->terms }}</textarea>
+				</p>
+			</div>
+
+			@component('partials.forms.buttons.primary')
+				Update
 			@endcomponent
-		
-		@else
 
-			<form role="form" method="POST" action="{{ route('invoices.update', $invoice->id) }}">
-				{{ csrf_field() }}
-				{{ method_field('PUT') }}
-
-				<div class="field">
-					<label class="label" for="created_at">Date Created</label>
-					<p class="control">
-						<input type="date" class="input" name="created_at" value="{{ $invoice->created_at->format('Y-m-d') }}" />
-					</p>
-				</div>
-
-				<div class="field">
-					<label class="label" for="due_at">Date Due</label>
-					<p class="control">
-						<input type="date" class="input" name="due_at" value="{{ $invoice->due_at ? $invoice->due_at->format('Y-m-d') : null }}" />
-					</p>
-				</div>
-
-				<div class="field">
-					<label class="label" for="number">Number</label>
-					<p class="control">
-						<input type="number" class="input" name="number" value="{{ $invoice->number }}" />
-					</p>
-				</div>
-
-				<div class="field">
-					<label class="label" for="user_id">Users</label>
-					<p class="control ix-expanded">
-						<select name="user_id[]" class="select2" multiple>
-							@foreach (users() as $user)
-								<option @if ($invoice->users->contains($user->id)) selected @endif value="{{ $user->id }}">{{ $user->name }}</option>
-							@endforeach
-						</select>
-					</p>
-				</div>
-
-				<div class="field">
-					<label class="label" for="recipient">Recipient</label>
-					<p class="control">
-						<textarea name="recipient" class="textarea">{{ $invoice->recipient }}</textarea>
-					</p>
-				</div>
-
-				<div class="field">
-					<label class="label" for="terms">Terms</label>
-					<p class="control">
-						<textarea name="terms" class="textarea">{{ $invoice->terms }}</textarea>
-					</p>
-				</div>
-
-				@component('partials.forms.buttons.primary')
-					Update
-				@endcomponent
-
-			</form>
-
-		@endif
+		</form>
 
 	@endcomponent
 
