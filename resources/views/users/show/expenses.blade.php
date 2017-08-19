2495b48a67a18a0d84f751808de4f5547b3d988b
= <=>
2017-08-19 23:09:46 +0100

    added users expenses list

---
+++ resources/views/users/show/expenses.blade.php
@@ -0,0 +1,49 @@
+@extends('layouts.app')
+
+@section('content')
+
+	<section class="section">
+		<div class="container">
+
+			<a href="{{ route('users.show', $user->id) }}" class="button is-pulled-right">
+				Return
+			</a>
+
+			<h1 class="title">{{ $user->name }}</h1>
+			<h2 class="subtitle">Expenses linked to this user</h2>
+
+			<hr />
+
+			<table class="table is-striped is-fullwidth">
+				<thead>
+					<th>Name</th>
+					<th>Property</th>
+					<th>Cost</th>
+					<th>Balance</th>
+					<th>Date</th>
+				</thead>
+				<tbody>
+					@foreach ($user->expenses as $expense)
+						<tr>
+							<td>
+								<a href="{{ route('expenses.show', $expense->id) }}">
+									{{ $expense->name }}
+								</a>
+							</td>
+							<td>
+								<a href="{{ route('properties.show', $expense->property->id) }}">
+									{{ $expense->property->short_name }}
+								</a>
+							</td>
+							<td>{{ currency($expense->cost) }}</td>
+							<td>{{ currency($expense->balance_amount) }}</td>
+							<td>{{ date_formatted($expense->created_at) }}</td>
+						</tr>
+					@endforeach
+				</tbody>
+			</table>
+
+		</div>
+	</section>
+
+@endsection
--- resources/views/users/show/layout.blade.php
+++ resources/views/users/show/layout.blade.php
@@ -133,56 +133,65 @@
 
 					</div>
 
-					<div class="box mb-2">
+					<div class="card mb-2">
+						<div class="card-content">
 
-						<h3 class="title">Recent Invoices</h3>
-						<h5 class="subtitle">The recent paid and unpaid invoices that this user is linked to.</h5>
+							<h3 class="title">Recent Invoices</h3>
+							<h5 class="subtitle">The recent paid and unpaid invoices that this user is linked to.</h5>
 
-						<table class="table is-striped is-fullwidth">
-							<thead>
-								<th>Number</th>
-								<th>Amount</th>
-								<th>Balance</th>
-								<th>Date</th>
-							</thead>
-							<tbody>
-								@foreach ($user->invoices()->limit(5)->get() as $invoice)
-									<tr>
-										<td><a href="{{ route('invoices.show', $invoice->id) }}">{{ $invoice->number }}</a></td>
-										<td>{{ currency($invoice->total) }}</td>
-										<td>{{ currency($invoice->total_balance) }}</td>
-										<td>{{ date_formatted($invoice->created_at) }}</td>
-									</tr>
-								@endforeach
-							</tbody>
-						</table>
+							<table class="table is-striped is-fullwidth">
+								<thead>
+									<th>Number</th>
+									<th>Amount</th>
+									<th>Balance</th>
+									<th>Date</th>
+								</thead>
+								<tbody>
+									@foreach ($user->invoices()->limit(5)->get() as $invoice)
+										<tr>
+											<td><a href="{{ route('invoices.show', $invoice->id) }}">{{ $invoice->number }}</a></td>
+											<td>{{ currency($invoice->total) }}</td>
+											<td>{{ currency($invoice->total_balance) }}</td>
+											<td>{{ date_formatted($invoice->created_at) }}</td>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
 
+						</div>
 					</div>
 
-					<div class="box mb-2">
+					<div class="card mb-2">
+						<div class="card-content">
 
-						<h3 class="title">Recent Expenses</h3>
-						<h5 class="subtitle">The recent paid and unpaid expenses linked to this user.</h5>
+							<h3 class="title">Recent Expenses</h3>
+							<h5 class="subtitle">The recent paid and unpaid expenses linked to this user.</h5>
 
-						<table class="table is-striped is-fullwidth">
-							<thead>
-								<th>Name</th>
-								<th>Cost</th>
-								<th>Balance</th>
-								<th>Date</th>
-							</thead>
-							<tbody>
-								@foreach ($user->expenses()->limit(5)->get() as $expense)
-									<tr>
-										<td><a href="{{ route('expenses.show', $expense->id) }}">{{ $expense->name }}</a></td>
-										<td>{{ currency($expense->cost) }}</td>
-										<td>{{ currency($expense->balance_amount) }}</td>
-										<td>{{ date_formatted($expense->created_at) }}</td>
-									</tr>
-								@endforeach
-							</tbody>
-						</table>
+							<table class="table is-striped is-fullwidth">
+								<thead>
+									<th>Name</th>
+									<th>Cost</th>
+									<th>Balance</th>
+									<th>Date</th>
+								</thead>
+								<tbody>
+									@foreach ($user->expenses()->limit(5)->get() as $expense)
+										<tr>
+											<td><a href="{{ route('expenses.show', $expense->id) }}">{{ $expense->name }}</a></td>
+											<td>{{ currency($expense->cost) }}</td>
+											<td>{{ currency($expense->balance_amount) }}</td>
+											<td>{{ date_formatted($expense->created_at) }}</td>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
 
+						</div>
+						<footer class="card-footer">
+							<a class="card-footer-item" href="{{ route('users.show', [$user->id, 'expenses']) }}">
+								Expenses List
+							</a>
+						</footer>
 					</div>
 
 				</div>
