9c078b138d84a1b95e01fc7b2ce128542edb1fa5
= <=>
2017-09-06 10:16:04 +0100

    remember me login now works. updated sent statement payments list

--- resources/views/statement-payments/partials/sent-payments-table.blade.php
+++ resources/views/statement-payments/partials/sent-payments-table.blade.php
@@ -1,7 +1,8 @@
 <table class="table table-striped table-responsive">
 	<thead>
 		<th>Property</th>
-		<th>Tenancy</th>
+		<th>Start</th>
+		<th>End</th>
 		<th>Name</th>
 		<th>Method</th>
 		<th>Amount</th>
@@ -12,10 +13,11 @@
 			<tr>
 				<td>{!! truncate($payment->statement->tenancy->property->short_name) !!}</td>
 				<td>
-					<a href="{{ route('tenancies.show', $payment->statement->tenancy->id) }}">
-						{!! truncate($payment->statement->tenancy->name) !!}
+					<a href="{{ route('statements.show', $payment->statement_id) }}">
+						{{ date_formatted($payment->statement->period_start) }}
 					</a>
 				</td>
+				<td>{{ date_formatted($payment->statement->period_end) }}</td>
 				<td>{{ $payment->name_formatted }}</td>
 				<td>@if ($payment->bank_account) Bank @else Cash or Cheque @endif</td>
 				<td>{{ currency($payment->amount) }}</td>
