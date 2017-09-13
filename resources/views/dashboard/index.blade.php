ef782295e1bced4966bf3560f8900c8d05ee2b7d
= <=>
2017-09-13 22:03:02 +0100

    Updated the Dashboard layout

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -6,24 +6,28 @@
 		<div class="container">
 
 			<div class="row">
-				<div class="col col-6">
+				<div class="col col-8">
 
-					<div class="card bg-danger mb-3">
-						<div class="card-header text-white">
+					<div class="card text-white bg-danger mb-3">
+						<div class="card-header">
 							{{ count($overdue_tenancies) }} Overdue {{ str_plural('Tenancy', count($overdue_tenancies)) }}
 						</div>
-						<ul class="list-group list-group-flush">
-							@foreach ($overdue_tenancies as $tenancy)
-								@component('partials.bootstrap.list-group-item')
-									<a href="{{ route('tenancies.show', $tenancy->id) }}" title="{{ $tenancy->name }}">
-										{{ $tenancy->name }}
-									</a>
-									@slot('title')
-										{{ $tenancy->days_overdue }} {{ str_plural('day', $tenancy->days_overdue) }}
-									@endslot
-								@endcomponent
-							@endforeach
-						</ul>
+						<table class="table table-striped table-responsive">
+							<thead>
+								<th>Overdue</th>
+								<th>Property</th>
+								<th>Tenancy</th>
+							</thead>
+							<tbody>
+								@foreach ($overdue_tenancies as $tenancy)
+									<tr>
+										<td>{{ $tenancy->days_overdue }} {{ str_plural('day', $tenancy->days_overdue) }}</td>
+										<td>{{ $tenancy->property->short_name }}</td>
+										<td>{{ $tenancy->name }}</td>
+									</tr>
+								@endforeach
+							</tbody>
+						</table>
 					</div>
 
 				</div>
