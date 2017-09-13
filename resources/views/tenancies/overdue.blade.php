0cc2936d7a19e27422981b2a2d0c8a847134e367
= <=>
2017-09-13 22:41:34 +0100

    Added extra fields to the overdue tenancies list

--- resources/views/tenancies/overdue.blade.php
+++ resources/views/tenancies/overdue.blade.php
@@ -18,21 +18,25 @@
 			<table class="table table-striped table-responsive">
 				<thead>
 					<th>Overdue</th>
-					<th>Property</th>
 					<th>Tenancy</th>
+					<th>Property</th>
+					<th>Rent</th>
+					<th>Balance</th>
 					<th>Last Payment</th>
 				</thead>
 				<tbody>
 					@foreach ($tenancies as $tenancy)
 						<tr>
 							<td>{{ $tenancy->days_overdue }} {{ str_plural('day', $tenancy->days_overdue) }}</td>
-							<td>{{ $tenancy->property->short_name }}</td>
 							<td>
 								<a href="{{ route('tenancies.show', $tenancy->id) }}" title="{{ $tenancy->name }}">
 									{{ $tenancy->name }}
 								</a>
 							</td>
-							<td>{{ $tenancy->last_rent_payment ? date_formatted($tenancy->last_rent_payment->created_at) : '' }}</td>
+							<td>{{ $tenancy->property->short_name }}</td>
+							<td>{{ $tenancy->current_rent ? currency($tenancy->current_rent->amount) : 'None' }}</td>
+							<td>{{ currency($tenancy->rent_balance) }}</td>
+							<td>{{ $tenancy->last_rent_payment ? date_formatted($tenancy->last_rent_payment->created_at) : 'Never' }}</td>
 						</tr>
 					@endforeach
 				</tbody>
