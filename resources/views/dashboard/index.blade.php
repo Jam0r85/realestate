a1bd68cd6926c0d201093d049a307206e3c77091
= <=>
2017-09-13 22:04:28 +0100

    Updated the Dashboard layout

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,7 +8,7 @@
 			<div class="row">
 				<div class="col col-8">
 
-					<div class="card text-white bg-danger mb-3">
+					<div class="card mb-3">
 						<div class="card-header">
 							{{ count($overdue_tenancies) }} Overdue {{ str_plural('Tenancy', count($overdue_tenancies)) }}
 						</div>
@@ -23,7 +23,11 @@
 									<tr>
 										<td>{{ $tenancy->days_overdue }} {{ str_plural('day', $tenancy->days_overdue) }}</td>
 										<td>{{ $tenancy->property->short_name }}</td>
-										<td>{{ $tenancy->name }}</td>
+										<td>
+											<a href="{{ route('tenancies.show', $tenancy->id) }}" title="{{ $tenancy->name }}">
+												{{ $tenancy->name }}
+											</a>
+										</td>
 									</tr>
 								@endforeach
 							</tbody>
