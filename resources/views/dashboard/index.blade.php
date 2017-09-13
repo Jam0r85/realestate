7d14171f98a64547bbc310cbe7bd2c37858b0a7e
= <=>
2017-09-13 22:33:55 +0100

    Updated overdue tenancies on dashboard and list

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -8,14 +8,19 @@
 			<div class="card-columns">
 
 				@if ($overdue_tenancies)
-					<div class="card bg-danger white-text mb-3">
+					<div class="card bg-danger text-white mb-3">
 						<div class="card-body text-center">
 							<h4 class="card-title">
 								Overdue Tenancies!
 							</h4>
 							<p class="card-text">
-								We have {{ count($overdue_tenancies) }} overdue {{ str_plural('tenancy', count($overdue_tenancies)) }}
+								We have {{ $overdue_tenancies }} overdue {{ str_plural('tenancy', $overdue_tenancies) }}
 							</p>
+						</div>
+						<div class="card-footer">
+							<a href="{{ route('tenancies.overdue') }}" title="Overdue Tenancies List">
+								View Overdue Tenancies
+							</a>
 						</div>
 					</div>
 				@endif
