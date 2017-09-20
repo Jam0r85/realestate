8bbb588cef399e78cb5f44cc193c3d97895963cd
= <=>
2017-09-20 15:31:03 +0100

    Update

--- app/Helpers/NumberHelper.php
+++ app/Helpers/NumberHelper.php
@@ -2,12 +2,11 @@
 
 function currency($amount = null)
 {
-	return $amount;
 	if (is_null($amount)) {
 		$amount = 0;
 	}
 	
 	setlocale(LC_MONETARY, 'en_GB');
-	// return '£' . money_format('%!n', $amount);
-	return '£' . number_format($amount, 2);
+	return '£' . money_format('%!n', $amount);
+	// return '£' . number_format($amount, 2);
 }
