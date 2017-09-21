946f9534e6212f8f992d2e22b04a5f8b0c1146d9
= <=>
2017-09-21 22:03:34 +0100

    Updated list group item partial

--- resources/views/partials/bootstrap/list-group-item.blade.php
+++ resources/views/partials/bootstrap/list-group-item.blade.php
@@ -1,7 +1,7 @@
 <li class="list-group-item {{ isset($style) ? $style : '' }} flex-column">
 	<div class="d-flex justify-content-between">
 		@if (isset($title))
-			<span class="text-muted">
+			<span class="{{ !isset($style) ? 'text-muted' : '' }}">
 				{{ $title }}
 			</span>
 		@endif
