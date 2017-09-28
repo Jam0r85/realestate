df085f757c85dce648055b7352beaff7a6624f5d
= <=>
2017-09-28 15:28:58 +0100

    Updated the statement payments table checkbox

--- resources/views/statement-payments/partials/payment-checkbox.blade.php
+++ resources/views/statement-payments/partials/payment-checkbox.blade.php
@@ -1,5 +1,5 @@
 <label class="custom-control custom-checkbox">
 	<input type="checkbox" class="custom-control-input" name="payments[]" value="{{ $payment->id }}" />
 	<span class="custom-control-indicator"></span>
-	<span class="custom-control-description"></span>
+	<span class="custom-control-description">Sent?</span>
 </label>
