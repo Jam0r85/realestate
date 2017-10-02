dcfd33a6059af2e64a6eb0b0f2699b459b87982e
= <=>
2017-10-02 11:36:54 +0100

    Bug fixes

--- app/Http/Controllers/GasController.php
+++ app/Http/Controllers/GasController.php
@@ -6,6 +6,7 @@ use App\Gas;
 use App\Http\Requests\StoreGasSafeReminderRequest;
 use App\Services\GasService;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class GasController extends BaseController
 {
