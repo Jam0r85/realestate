ea35e19dd89270bcaf58e82af64b892ec9601134
= <=>
2017-10-03 10:53:04 +0100

    Fixed a bug on the statement payments table

--- resources/views/statement-payments/index.blade.php
+++ resources/views/statement-payments/index.blade.php
@@ -12,7 +12,7 @@
 		</div>
 	</section>
 
-	@if (count($unsent_payments)  && $payments->currentPage() == 1)
+	@if (count($unsent_payments)  && $unsent_payments->currentPage() == 1)
 		<section class="section">
 			<div class="container">
 
