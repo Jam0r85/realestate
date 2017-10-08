7f9b88fcb29fef31bd1bef7af8d857ed386da12b
= <=>
2017-10-08 13:20:36 +0100

    Updated dashboard styling

--- resources/views/dashboard/cards/income.blade.php
+++ resources/views/dashboard/cards/income.blade.php
@@ -8,12 +8,14 @@
 				{{ currency($commission) }}
 			</span>
 			Commission
+			<br /><small class="text-muted">Management, letting and re-letting fees</small>
 		</div>
 		<div class="list-group-item">
 			<span class="float-right">
 				{{ currency($invoice_income) }}
 			</span>
-			Invoices
+			Invoice Income
+			<br /><small class="text-muted">Application invoices, sale invoices, etc</small>
 		</div>
 		<div class="list-group-item">
 			<span class="float-right">
