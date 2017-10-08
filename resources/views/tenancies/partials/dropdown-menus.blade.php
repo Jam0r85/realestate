08b6d9c335a17387baa60b21d2ff4da1bad1d0d3
= <=>
2017-10-08 17:46:00 +0100

    Updated tenancies dropdown menu

--- resources/views/tenancies/partials/dropdown-menus.blade.php
+++ resources/views/tenancies/partials/dropdown-menus.blade.php
@@ -18,15 +18,8 @@
 		<a class="dropdown-item" href="{{ route('tenancies.show', [$tenancy->id, 'record-old-statement']) }}" title="Record Old Statement">
 			Record Old Statement
 		</a>
-	</div>
-</div>
-
-<div class="btn-group">
-	<button class="btn btn-danger dropdown-toggle" type="button" id="tenanciesActionsDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-		Actions
-	</button>
-	<div class="dropdown-menu dropdown-menu-right" aria-labelledby="tenanciesActionsDropdown">
-		<a class="dropdown-item" href="{{ route('tenancies.show', [$tenancy->id, 'tenants-vacated']) }}">
+		<div class="dropdown-divider"></div>
+		<a class="dropdown-item" href="{{ route('tenancies.show', [$tenancy->id, 'tenants-vacated']) }}" title="Tenants Vacting">
 			Tenants Vacated
 		</a>
 	</div>
