db0b297b97d0578767e0f65267b9600261628af1
= <=>
2017-10-08 15:38:47 +0100

    Cleaned up the deposit header

--- resources/views/deposits/index.blade.php
+++ resources/views/deposits/index.blade.php
@@ -2,59 +2,53 @@
 
 @section('content')
 
-	<section class="section">
-		<div class="container">
+	@component('partials.bootstrap.section-with-container')
 
-			<div class="page-title">
-				<div class="float-right">
+		<div class="page-title">
+			<h1>
+				{{ $title }}
+			</h1>
+		</div>
 
-				</div>
-				<h1>
-					{{ $title }}
-					<span class="badge badge-info">
-						{{ currency($deposits->sum('amount')) }}
-					</span>
-					@if (isset($deposit_balance))
-						<span class="badge badge-success">
-							{{ currency($deposit_balance) }}
-						</span>
-					@endif
-				</h1>
-			</div>
+		<div class="btn-group mb-3">
+			<button type="button" class="btn">
+				Total <span class="badge badge-secondary">{{ currency($deposits->sum('amount')) }}</span>
+			</button>
+			<button type="button" class="btn">
+				Balance <span class="badge badge-secondary">{{ currency($deposit_balance) }}</span>
+			</button>
+		</div>
 			
-			<div class="page-search">
-				<form role="form" method="POST" action="{{ route('deposit.search') }}">
-					{{ csrf_field() }}
-					<div class="form-group">
-						<div class="input-group">
-							{{-- Clear Search Button --}}
-							@if (session('deposit_search_term'))
-								<span class="input-group-btn">
-									<button type="submit" class="btn btn-danger" name="clear_search" value="true">
-										<i class="fa fa-trash"></i> Clear
-									</button>
-								</span>
-							@endif
-							<input type="text" name="search_term" class="form-control" placeholder="Search for..." value="{{ session('deposit_search_term') }}" />
+		<div class="page-search">
+			<form role="form" method="POST" action="{{ route('deposit.search') }}">
+				{{ csrf_field() }}
+				<div class="form-group">
+					<div class="input-group">
+						{{-- Clear Search Button --}}
+						@if (session('deposit_search_term'))
 							<span class="input-group-btn">
-								<button type="submit" class="btn btn-secondary">
-									<i class="fa fa-search"></i> Search
+								<button type="submit" class="btn btn-danger" name="clear_search" value="true">
+									<i class="fa fa-trash"></i> Clear
 								</button>
 							</span>
-						</div>
+						@endif
+						<input type="text" name="search_term" class="form-control" placeholder="Search for..." value="{{ session('deposit_search_term') }}" />
+						<span class="input-group-btn">
+							<button type="submit" class="btn btn-secondary">
+								<i class="fa fa-search"></i> Search
+							</button>
+						</span>
 					</div>
-				</form>
-			</div>
-
+				</div>
+			</form>
 		</div>
-	</section>
 
-	<section class="section">
-		<div class="container">
+	@endcomponent
 
-			@include('deposits.partials.table')
+	@component('partials.bootstrap.section-with-container')
 
-		</div>
-	</section>
+		@include('deposits.partials.table')
+
+	@endcomponent
 
 @endsection
