a0d92a8e1be4ecd13393343221241982bb7944ba
= <=>
2017-10-09 10:56:59 +0100

    Updated payment info card

--- resources/views/payments/partials/payment-info-card.blade.php
+++ resources/views/payments/partials/payment-info-card.blade.php
@@ -1,6 +1,6 @@
 <div class="card mb-3">
 	<div class="card-header">
-		<i class="fa fa-user"></i> User's Information
+		<i class="fa fa-gbp"></i> Payment Information
 	</div>
 	<ul class="list-group list-group-flush">
 		@component('partials.bootstrap.list-group-item')
