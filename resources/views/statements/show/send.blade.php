6c5b36af00f0d2b813e1161829d08bf2e9077247
= <=>
2017-10-16 14:26:40 +0100

    Updated resend statement blade tempalte

--- resources/views/statements/show/send.blade.php
+++ resources/views/statements/show/send.blade.php
@@ -2,60 +2,62 @@
 
 @section('content')
 
-	<section class="section">
-		<div class="container">
+	@component('partials.bootstrap.section-with-container')
 
-			<div class="page-title">
-				<a href="{{ route('statements.show', $statement->id) }}" class="btn btn-secondary float-right">
-					Return
-				</a>
-				<h1>Statement #{{ $statement->id }}</h1>
-				<h3>Send the statement to it's owners.</h3>
-			</div>
-
-			@if ($statement->sent_at)
-				<div class="alert alert-success">
-					The statement was sent to it's owners on <b>{{ date_formatted($statement->sent_at) }}</b>
-				</div>
-			@endif
-
-			<div class="alert @if (!$statement->getUserEmails()) alert-danger @else alert-info @endif">
-				@if (!$statement->getUserEmails())
-					The users assigned to this statement do not have valid e-mail addresses.
-				@else
-					Statement Recipients:
-					<ul>
-						@foreach ($statement->users as $user)
-							<li>
-								{{ $user->name }} ({{ $user->email }})
-							</li>
-						@endforeach
-					</ul>
-				@endif
-			</div>
-
-			<div class="alert alert-info">
-				Statements are usual sent to the owners of this property by 
-				@if ($statement->sendByPost())
-					Post
-				@endif
-				@if ($statement->sendByEmail())
-					E-Mail
-				@endif
-			</div>
-
-			<form role="form" method="POST" action="{{ route('statements.resend', $statement->id) }}">
-				{{ csrf_field() }}
-
-				<input type="hidden" name="statement_id" value="{{ $statement->id }}" />
-
-				<button type="submit" class="btn btn-primary" @if (!$statement->getUserEmails()) disabled @endif>
-					<i class="fa fa-envelope-open"></i> Send Statement By E-Mail
-				</button>
-
-			</form>
-
+		<div class="page-title">
+			<a href="{{ route('statements.show', $statement->id) }}" class="btn btn-secondary float-right">
+				Return
+			</a>
+			<h1>Statement #{{ $statement->id }}</h1>
+			<h3>{{ $statement->sent_at ? 'Resend' : 'Send' }} the statement to it's owners.</h3>
 		</div>
-	</section>
+
+	@endcomponent
+
+	@component('partials.bootstrap.section-with-container')
+
+		@if ($statement->sent_at)
+			<div class="alert alert-success lead">
+				This statement has already been sent to it's recipients on <b>{{ date_formatted($statement->sent_at) }}</b>
+			</div>
+		@endif
+
+		<div class="card mb-3 border-primary">
+			<div class="card-header bg-primary text-white">
+				{{ $statement->sent_at ? 'Resend' : 'Sent' }} Statement
+			</div>
+			<div class="card-body">
+
+				<p class="card-text">
+					You can {{ $statement->sent_at ? 'resend' : 'Send' }} this statement to the following recipients by e-mail.
+				</p>
+
+				<ul class="mb-5">
+					@foreach ($statement->users as $user)
+						<li>
+							{{ $user->email }} ({{ $user->name }})
+						</li>
+					@endforeach
+				</ul>
+
+				<form method="POST" action="{{ route('statements.resend', $statement->id) }}">
+					{{ csrf_field() }}
+
+					<input type="hidden" name="statement_id" value="{{ $statement->id }}" />
+
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary">
+							<i class="fa fa-send fa-fw"></i> Resend Statement
+						</button>
+					</div>
+
+				</form>
+
+				<em>Note that recipients without an e-mail address will not receive anything.</em>
+
+			</div>
+		</div>
+
+	@endcomponent
 
 @endsection
