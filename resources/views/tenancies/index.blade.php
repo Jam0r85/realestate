aee5ea84a24f143006617bf3e7586e02b5d21043
= <=>
2017-10-18 20:50:53 +0100

    Updated tenancies table

--- resources/views/tenancies/index.blade.php
+++ resources/views/tenancies/index.blade.php
@@ -2,40 +2,65 @@
 
 @section('content')
 
-	<section class="section">
-		<div class="container">
+	@component('partials.bootstrap.section-with-container')
 
-			<div class="page-title">
-				<h1>
-					{{ $title }}
-					<a href="{{ route('tenancies.create') }}" class="btn btn-primary">
-						<i class="fa fa-plus"></i> New Tenancy
-					</a>
-				</h1>
-			</div>
+		<div class="page-title">
+			<h1>
+				{{ $title }}
+				<a href="{{ route('tenancies.create') }}" class="btn btn-primary">
+					<i class="fa fa-plus"></i> New Tenancy
+				</a>
+			</h1>
+		</div>
 
-			{{-- Tenancies Search --}}
-			@component('partials.bootstrap.page-search')
-				@slot('route')
-					{{ route('tenancies.search') }}
+		{{-- Tenancies Search --}}
+		@component('partials.bootstrap.page-search')
+			@slot('route')
+				{{ route('tenancies.search') }}
+			@endslot
+			@if (session('tenancies_search_term'))
+				@slot('search_term')
+					{{ session('tenancies_search_term') }}
 				@endslot
-				@if (session('tenancies_search_term'))
-					@slot('search_term')
-						{{ session('tenancies_search_term') }}
-					@endslot
-				@endif
-			@endcomponent
-			{{-- End of Tenancies Search --}}
+			@endif
+		@endcomponent
+		{{-- End of Tenancies Search --}}
 
-		</div>
-	</section>
+	@endcomponent
 
-	<section class="section">
-		<div class="container">
+	@component('partials.bootstrap.section-with-container')
 
-			@include('tenancies.partials.table')
+		<table class="table table-striped table-hover table-responsive">
+			<thead>
+				<th>Name</th>
+				<th>Property</th>
+				<th>Rent</th>
+				<th>Balance</th>
+				<th>Service</th>
+				<th>Status</th>
+			</thead>
+			<tbody>
+				@foreach ($tenancies as $tenancy)
+					<tr>
+						<td>
+							<a href="{{ route('tenancies.show', $tenancy->id) }}" title="{{ $tenancy->name }}">
+								{!! truncate($tenancy->name) !!}
+							</a>
+						</td>
+						<td>{!! truncate($tenancy->property->short_name) !!}</td>
+						<td>{{ currency($tenancy->rent_amount) }}</td>
+						<td>
+							@include('tenancies.format.rent-balance')
+						</td>
+						<td>{{ $tenancy->service->name }}</td>
+						<td>@include('tenancies.partials.table-status-label')</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
 
-		</div>
-	</section>
+		@include('partials.pagination', ['collection' => $tenancies])
+
+	@endcomponent
 
 @endsection
---
+++ resources/views/tenancies/partials/table-status-label.blade.php
@@ -0,0 +1,27 @@
+@if (!is_null($tenancy->vacated_on) && ($tenancy->vacated_on <= \Carbon\Carbon::now()))
+	<span class="badge badge-danger">
+		Vacated
+	</span>
+@endif
+
+@if (!is_null($tenancy->vacated_on) && ($tenancy->vacated_on > \Carbon\Carbon::now()))
+	<span class="badge badge-warning">
+		Vacating
+	</span>
+@endif
+
+@if (is_null($tenancy->vacated_on))
+
+	<span class="badge badge-success">
+		Active
+	</span>
+
+	@if (!$tenancy->deposit)
+
+		<span class="badge badge-warning">
+			No Deposit
+		</span>
+
+	@endif
+
+@endif
