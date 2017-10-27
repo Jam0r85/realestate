651dc2dcd74534145a3073bbfdc03a255ab731b1
= <=>
2017-10-27 15:26:55 +0100

    updated statement payments list

--- resources/views/statement-payments/index.blade.php
+++ resources/views/statement-payments/index.blade.php
@@ -70,7 +70,34 @@
 
 		</div>
 
-		@include('statement-payments.partials.sent-payments-table', ['payments' => $sent_payments])
+		<table class="table table-striped table-responsive">
+			<thead>
+				<th>Property</th>
+				<th>Statement</th>
+				<th>Name</th>
+				<th>Method</th>
+				<th>Amount</th>
+				<th>Sent</th>
+			</thead>
+			<tbody>
+				@foreach ($sent_payments as $payment)
+					<tr>
+						<td>{!! truncate($payment->statement->tenancy->property->short_name) !!}</td>
+						<td>
+							<a href="{{ route('statements.show', $payment->statement->id) }}">
+								{{ $payment->statement->id }}
+							</a>
+						</td>
+						<td>{{ $payment->name_formatted }}</td>
+						<td>{{ $payment->bank_account ? 'Bank' : 'Cheque' }}</td>
+						<td>{{ currency($payment->amount) }}</td>
+						<td>{{ date_formatted($payment->sent_at) }}</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+		@include('partials.pagination', ['collection' => $sent_payments])
 
 	@endcomponent
 
