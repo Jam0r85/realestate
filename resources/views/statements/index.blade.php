5f207f6020d03887c87dc4128e692ccdc8460e77
= <=>
2017-10-29 23:24:55 +0000

    updated statements index

--- resources/views/statements/index.blade.php
+++ resources/views/statements/index.blade.php
@@ -2,64 +2,65 @@
 
 @section('content')
 
-	<section class="section">
-		<div class="container">
+	@component('partials.bootstrap.section-with-container')
 
-			<div class="page-title">
-				<h1>{{ $title }}</h1>
-			</div>
+		<div class="page-title">
 
-			{{-- Users Search --}}
-			@component('partials.bootstrap.page-search')
-				@slot('route')
-					{{ route('statements.search') }}
-				@endslot
-				@if (session('statements_search_term'))
-					@slot('search_term')
-						{{ session('statements_search_term') }}
-					@endslot
-				@endif
+			@component('partials.header')
+				{{ $title }}
 			@endcomponent
-			{{-- End of Users Search --}}
 
 		</div>
-	</section>
+
+		{{-- Statements search form --}}
+		@component('partials.bootstrap.page-search')
+			@slot('route')
+				{{ route('statements.search') }}
+			@endslot
+			@if (session('statements_search_term'))
+				@slot('search_term')
+					{{ session('statements_search_term') }}
+				@endslot
+			@endif
+		@endcomponent
+		{{-- End statements search form --}}
+
+	@endcomponent
 
 	@if (isset($unsent_statements))
 		@if (count($unsent_statements) && $statements->currentPage() == 1)
-			<section class="section">
-				<div class="container">
-					<h3 class="text-danger">
-						Unsent and/or Unpaid Statements
-					</h3>
-					<div class="row">
-						<div class="col">
-							@include('statements.partials.unsent-statements-table', ['statements' => $unsent_statements])
-						</div>
-					</div>
+
+			@component('partials.bootstrap.section-with-container')
+
+				<div class="page-title">
+
+					@component('partials.sub-header')
+						Unsent ando/or Unpaid Statements
+					@endcomponent
+
 				</div>
-			</section>
+
+				@include('statements.partials.unsent-statements-table', ['statements' => $unsent_statements])
+
+			@endcomponent
+
 		@endif
 	@endif
 
-	<section class="section">
-		<div class="container">
+	@component('partials.bootstrap.section-with-container')
 
-			@if (!session('statements_search_term'))
-				<div class="page-title">
-					<h3 class="text-success">
-						Sent Statements
-					</h3>
-				</div>
-			@endif
+		@if (!session('statements_search_term'))
+			<div class="page-title">
 
-			<div class="row">
-				<div class="col">
-					@include('statements.partials.sent-statements-table')
-				</div>
+				@component('partials.sub-header')
+					Sent Statements
+				@endcomponent
+
 			</div>
+		@endif
 
-		</div>
-	</section>
+		@include('statements.partials.sent-statements-table')
+
+	@endcomponent
 
 @endsection
--- resources/views/statements/partials/sent-statements-table.blade.php
+++ resources/views/statements/partials/sent-statements-table.blade.php
@@ -1,9 +1,9 @@
-<table class="table table-striped table-responsive">
+<table class="table table-striped table-hover table-responsive">
 	<thead>
-		<th>Tenancy</th>
-		<th>Property</th>
 		<th>Starts</th>
 		<th>Ends</th>
+		<th>Tenancy</th>
+		<th>Property</th>
 		<th>Amount</th>
 		<th>Sent</th>
 	</thead>
@@ -11,17 +11,17 @@
 		@foreach ($statements as $statement)
 			<tr>
 				<td>
-					<a href="{{ route('tenancies.show', $statement->tenancy->id) }}" title="{{ $statement->tenancy->name }}">
-						{!! truncate($statement->tenancy->name) !!}
-					</a>
-				</td>
-				<td>{!! truncate($statement->property->short_name) !!}</td>
-				<td>
 					<a href="{{ route('statements.show', $statement->id) }}" title="Statement #{{ $statement->id }}">
 						{{ date_formatted($statement->period_start) }}
 					</a>
 				</td>
 				<td>{{ date_formatted($statement->period_end) }}</a>
+				<td>
+					<a href="{{ route('tenancies.show', $statement->tenancy->id) }}" title="{{ $statement->tenancy->name }}">
+						{!! truncate($statement->tenancy->name) !!}
+					</a>
+				</td>
+				<td>{!! truncate($statement->property->short_name) !!}</td>
 				</td>
 				<td>{{ currency($statement->amount) }}</td>
 				<td>{{ date_formatted($statement->sent_at) }}</td>
