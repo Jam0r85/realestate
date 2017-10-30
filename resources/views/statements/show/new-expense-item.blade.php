408178a5a07f8b42abb3d261fe2bd8626010070e
= <=>
2017-10-30 11:33:25 +0000

    updated statement new expense item alyout

--- resources/views/statements/show/new-expense-item.blade.php
+++ resources/views/statements/show/new-expense-item.blade.php
@@ -2,83 +2,73 @@
 
 @section('content')
 
-	<section class="section">
-		<div class="container">
+	@component('partials.bootstrap.section-with-container')
 
-			<a href="{{ route('statements.show', $statement->id) }}" class="button is-pulled-right">
-				Return
-			</a>
+		<a href="{{ route('statements.show', $statement->id) }}" class="button is-pulled-right">
+			Return
+		</a>
 
-			<h1 class="title">Statement #{{ $statement->id}}</h1>
-			<h2 class="subtitle">New expense item</h2>
+		@component('partials.header')
+			Statement #{{ $statement->id }}
+		@endcomponent
 
-			<hr />
+		@component('partials.sub-header')
+			New expense item
+		@endcomponent
 
-			@include('partials.errors-block')
+	@endcomponent
 
-			<form role="form" method="POST" action="{{ route('statements.create-expense-item', $statement->id) }}">
-				{{ csrf_field() }}
-				<input type="hidden" name="property_id" value="{{ $statement->tenancy->property->id }}" />	
+	@component('partials.bootstrap.section-with-container')
 
-				<div class="card mb-2">
-					<div class="card-content">
+		@include('partials.errors-block')
 
-						<h3 class="title">Unpaid expenses</h3>
-						<h5 class="subtitle">List of unpaid expenses linked to the property of this statement.</h5>
+		<form method="POST" action="{{ route('statements.create-expense-item', $statement->id) }}">
+			{{ csrf_field() }}
+			<input type="hidden" name="property_id" value="{{ $statement->tenancy->property->id }}" />	
 
-						<table class="table is-striped is-fullwidth">
-							<thead>
-								<th></th>
-								<th>Name</th>
-								<th>Contractors</th>
-								<th>Cost</th>
-								<th>Balance</th>
-							</thead>
-							<tbody>
-								@foreach ($statement->property->unpaid_expenses as $unpaid_expense)
-									<tr>
-										<td>
-											<div class="control has-icons-left">
-												<input type="number" step="any" name="expense_amount[]" class="input" />
-												<span class="icon is-small is-left">
-													<i class="fa fa-gbp"></i>
-												</span>
-											</div>
-											<input type="hidden" name="expense_id[]" value="{{ $unpaid_expense->id }}" />
-										</td>
-										<td>{{ $unpaid_expense->name }}</td>
-										<td>
-											@foreach ($unpaid_expense->contractors as $user)
-												<a href="{{ route('users.show', $user->id) }}">
-													<span class="tag is-primary">
-														{{ $user->name }}
-													</span>
-												</a>
-											@endforeach
-										</td>
-										<td>{{ currency($unpaid_expense->cost) }}</td>
-										<td>{{ currency($unpaid_expense->balance_amount) }}</td>
-									</tr>
-								@endforeach
-							</tbody>
-						</table>
+			<div class="card mb-3">
 
-					</div>
+				@component('partials.card-header')
+					Unpaid Expenses
+				@endcomponent
+
+				<div class="card-body">
+
+					<table class="table table-striped table-hover table-responsive">
+						<thead>
+							<th></th>
+							<th>Name</th>
+							<th>Contractors</th>
+							<th>Cost</th>
+							<th>Balance</th>
+						</thead>
+						<tbody>
+							@foreach ($statement->property->unpaid_expenses as $unpaid_expense)
+								<tr>
+									<td>
+										<input type="number" step="any" name="expense_amount[]" class="form-control" />
+										<input type="hidden" name="expense_id[]" value="{{ $unpaid_expense->id }}" />
+									</td>
+									<td>{{ $unpaid_expense->name }}</td>
+									<td>
+										@include('partials.bootstrap.users-inline', ['users' => $unpaid_expense->contractors])
+									</td>
+									<td>{{ currency($unpaid_expense->cost) }}</td>
+									<td>{{ currency($unpaid_expense->balance_amount) }}</td>
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
+
 				</div>
+			</div>
 
-				@include('expenses.partials.form')
+			@component('partials.save-submit')
+				Save Changes
+			@endcomponent
+			
+		</form>
 
-				<button type="submit" class="button is-primary">
-					<span class="icon is-small">
-						<i class="fa fa-save"></i>
-					</span>
-					<span>
-						Add &amp; Create Item(s)
-					</span>
-				</button>
-			</form>
-
-		</div>
-	</section>
+	@endcomponent
 
 @endsection
