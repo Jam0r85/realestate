7326d384014c4f307c60d96a3fcec4005173e8ab
= <=>
2017-11-05 10:44:00 +0000

    added dark style to cards

--- resources/views/partials/bootstrap/card-header.blade.php
+++ resources/views/partials/bootstrap/card-header.blade.php
@@ -1,4 +1,4 @@
-<div class="card-header {{ isset($style) ? $style : '' }}">
+<div class="card-header {{ isset($style) ? $style : '' }} {{ user_setting(Auth::user(), 'dark_mode') ? 'bg-dark' : '' }}">
 	{{ $slot }}
 
 	@if (isset($small))
