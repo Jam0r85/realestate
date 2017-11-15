89ed8963976af546b64afe781ab9e9ec7518a602
= <=>
2017-11-15 17:04:11 +0000

    updated expense

--- resources/views/expenses/create.blade.php
+++ resources/views/expenses/create.blade.php
@@ -2,11 +2,11 @@
 
 @section('content')
 
-	@component('partials.bootstrap.section-with-container')
+	@component('partials.page-header')
 
-		<div class="page-title">
-			<h1>Create Expense</h1>
-		</div>
+		@component('partials.header')
+			Create Expense
+		@endcomponent
 
 	@endcomponent
 
@@ -27,33 +27,53 @@
 						</option>
 					@endforeach
 				</select>
+				<small class="form-text text-muted">
+					Please select the property that this expense is for.
+				</small>
 			</div>
 
 			<div class="form-group">
 				<label for="name">Expense Name</label>
 				<input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}" />
+				<small class="form-text text-muted">
+					Enter a name for this expense. This will appear on the rental statement.
+				</small>
 			</div>
 
 			<div class="form-group">
 				<label for="cost">Expense Cost</label>
-				<input class="form-control" type="number" step="any" name="cost" id="cost" value="{{ old('cost') }}" />
+				<div class="input-group">
+					<span class="input-group-addon">
+						<i class="fa fa-gbp"></i>
+					</span>
+					<input class="form-control" type="number" step="any" name="cost" id="cost" value="{{ old('cost') }}" />
+				</div>
+				<small class="form-text text-muted">
+					Enter the full cost of the expense.
+				</small>
 			</div>
 
 			<div class="form-group">
 				<label for="contractor_id">Contractor</label>
 				<select name="contractor_id" id="contractor_id" class="form-control select2">
-					<option value="">Please select..</option>
+					<option value="">None</option>
 					@foreach (users() as $user)
 						<option @if (old('contractor_id') && old('contractor_id') == $user->id)) selected @endif value="{{ $user->id }}">
 							{{ $user->name }}
 						</option>
 					@endforeach
 				</select>
+				<small class="form-text text-muted">
+					Select the contractors to be attached to this expense.
+				</small>
 			</div>
 
 			<div class="form-group">
-				<label for="contractor_reference">Contractor Reference (Invoice Number)</label>
+				<label for="contractor_reference">Invoice Number (optional)</label>
 				<input class="form-control" type="text" name="contractor_reference" id="contractor_reference" value="{{ old('contractor_reference') }}" />
+				<small class="form-text text-muted">
+					Enter the invoice or reference number for this expense received from the contractor.
+				</small>
 			</div>
 
 			<div class="form-group">
@@ -61,7 +81,7 @@
 				<input type="file" id="files" class="form-control-file" name="files[]" multiple />
 			</div>
 
-			@component('partials.bootstrap.save-submit-button')
+			@component('partials.save-button')
 				Create Expense
 			@endcomponent
 
