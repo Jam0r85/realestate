1b84160bf6facaacc9b3d191ab4dddf5910815a6
= <=>
2017-11-16 18:40:48 +0000

    bug fix

--- resources/views/pdf/_header.blade.php
+++ resources/views/pdf/_header.blade.php
@@ -3,7 +3,6 @@
 		<base href="{{ url('/') }}">
 		<title>{{ isset($title) ? $title : '' }}</title>
 	    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.1/css/bulma.min.css" />
 
 	    <style type="text/css">
 			@page { margin: 100px 25px; }
