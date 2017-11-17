34daaf669ae51baba72146c0fecd484d987a73f1
= <=>
2017-11-17 13:19:55 +0000

    updated setting layout

--- resources/views/settings/general.blade.php
+++ resources/views/settings/general.blade.php
@@ -13,14 +13,6 @@
 			<input type="text" name="company_name" class="form-control" value="{{ get_setting('company_name') }}" />
 		</div>
 
-		<div class="form-group">
-			<label for="vat_number">VAT Number</label>
-			<input type="text" name="vat_number" class="form-control" value="{{ get_setting('vat_number') }}" />
-			<small class="form-text text-muted">
-				VAT registered? Enter your number to appear on invoices and statements.
-			</small>
-		</div>
-
 		<button type="submit" class="btn btn-primary">
 			<i class="fa fa-save"></i> Save Changes
 		</button>
