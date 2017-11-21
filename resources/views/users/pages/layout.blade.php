c1d59dfd442350fd6b9aa8d2606e6f0bfd587bd9
= <=>
2017-11-21 15:56:36 +0000

    updated layout

--- resources/views/users/pages/layout.blade.php
+++ resources/views/users/pages/layout.blade.php
@@ -28,7 +28,7 @@
 	@component('partials.bootstrap.section-with-container')
 
 		<div class="row">
-			<div class="col-12 col-md-4 col-lg-2">
+			<div class="col-12 col-md-4 col-lg-3 col-xl-2">
 
 				<div class="nav flex-column nav-pills mb-5" id="v-pills-tab" role="tablist" aria-orientation="vertical">
 
@@ -54,7 +54,7 @@
 				</div>
 
 			</div>
-			<div class="col-12 col-md-8 col-lg-10">
+			<div class="col-12 col-md-8 col-lg-9 col-xl-10">
 
 				<div class="tab-content" id="v-pills-tabContent">
 
