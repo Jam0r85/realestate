51e809e5e1fa9957c4a855394cb0ebe902d98de6
= <=>
2017-11-21 14:53:37 +0000

    git add .

--- resources/views/users/sections/invoices.blade.php
+++ resources/views/users/sections/invoices.blade.php
@@ -1,5 +1,11 @@
+@php
+	$invoices = $user->invoices()->paginate();
+@endphp
+
 <div class="tab-pane fade" id="v-pills-invoices" role="tabpanel">
 
-	@include('invoices.partials.invoices-table', ['invoices' => $user->invoices])
+	@include('invoices.partials.invoices-table', ['invoices' => $invoices])
+
+	@include('partials.pagination', ['collection' => $invoices])
 	
 </div>
