ac6709536c4edbd2db7a3cd0881893a6e92a1de2
= <=>
2017-11-21 13:44:09 +0000

    updated order of sms relations for users

--- app/User.php
+++ app/User.php
@@ -188,7 +188,7 @@ class User extends Authenticatable
      */
     public function sms()
     {
-        return $this->hasMany('App\SmsHistory');
+        return $this->hasMany('App\SmsHistory')->latest();
     }
 
     /**
