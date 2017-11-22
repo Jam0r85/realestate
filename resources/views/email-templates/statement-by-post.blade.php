ce9288674269f9f31ab02689cb8593795d39be11
= <=>
2017-11-22 16:38:52 +0000

    bug fix

--- resources/views/email-templates/statement-by-post.blade.php
+++ resources/views/email-templates/statement-by-post.blade.php
@@ -3,7 +3,7 @@
 
 Quick email to let you know that the latest rental statement for <b>{{ $statement->property()->present()->fullAddress }}</b> has been posted to you today.
 
-The amount of {{ currency($statement->getLandlordAmount) }} was sent to you by {{ $statement->bank_account ? 'Bank Transfer' : 'Cheque' }}.
+The amount of {{ currency($statement->getLandlordAmount()) }} was sent to you by {{ $statement->bank_account ? 'Bank Transfer' : 'Cheque' }}.
 
 Thanks,<br>
 {{ get_setting('company_name', config('app.name')) }}
