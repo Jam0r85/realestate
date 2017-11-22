789085d91b4091a93915c7e98939051d4fa49a9c
= <=>
2017-11-22 10:06:15 +0000

    updated eager loading

--- resources/views/tenancies/sections/statements.blade.php
+++ resources/views/tenancies/sections/statements.blade.php
@@ -1,5 +1,5 @@
 @php
-	$statements = $tenancy->statements()->with('invoices','invoices.items','invoices.items.taxRate')->paginate();
+	$statements = $tenancy->statements()->with('invoices','invoices.invoiceGroup','invoices.items','invoices.items.taxRate','expenses')->paginate();
 	$statements->appends(['section' => 'statements']);
 @endphp
 
