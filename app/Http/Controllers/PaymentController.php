e956156a666739841a289b0732a3ba7b0e11d818
= <=>
2017-12-11 13:50:02 +0000

    updated payment controller

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -5,47 +5,17 @@ namespace App\Http\Controllers;
 use App\Events\Tenancies\TenancyUpdateStatus;
 use App\Http\Requests\DestroyPaymentRequest;
 use App\Http\Requests\UpdatePaymentRequest;
-use App\Payment;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class PaymentController extends BaseController
 {
     /**
-     * Display a listing of all rent payments received.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function rentPayments()
-    {
-        $payments = Payment::where('parent_type', 'tenancies')->latest()->paginate();
-        $title = 'Rent Payments';
-
-        return view('payments.rent', compact('payments','title'));
-    }
-
-    /**
-     * Search through the resource and display the results.
+     * The eloquent model for this controller.
      * 
-     * @param  \Illuminate\Http\Requests $request
-     * @return \Illuminate\Http\Response
+     * @var string
      */
-    public function search(Request $request)
-    {
-        // Clear the search term.
-        if ($request && $request->has('clear_search')) {
-            Session::forget('payments_search_term');
-            return back();
-        }
-
-        Session::put('payments_search_term', $request->search_term);
-
-        $payments = Payment::search(Session::get('payments_search_term'))->get();
-        $payments->sortByDesc('created_at');
-        $title = 'Search Results';
-
-        return view('payments.rent', compact('payments','title'));
-    }
+    public $model = 'App\Payment';
 
     /**
      * Find and display the payment.
@@ -55,7 +25,9 @@ class PaymentController extends BaseController
      */
     public function show($id, $section = 'layout')
     {
-        $payment = Payment::findOrFail($id);
+        $payment = $this->repository
+            ->findOrFail($id);
+
         return view('payments.show.' . $section, compact('payment'));
     }
 
@@ -66,12 +38,12 @@ class PaymentController extends BaseController
      * @param  \App\Payment  $payment
      * @return \Illuminate\Http\Response
      */
-    public function update(UpdatePaymentRequest $request, Payment $payment)
+    public function update(UpdatePaymentRequest $request, $id)
     {
-        $payment->created_at = $request->created_at;
-        $payment->amount = $request->amount;
-        $payment->payment_method_id = $request->payment_method_id;
-        $payment->save();
+        $payment = $this->repository
+            ->findOrFail($id)
+            ->fill($request->input())
+            ->save();
 
         $payment->users()->sync($request->users);
 
@@ -82,18 +54,5 @@ class PaymentController extends BaseController
         }
 
         return back();
-    }
-
-    /**
-     * Destroy the given payment.
-     * 
-     * @param \App\Http\Requests\DestroyPaymentRequest $request
-     * @param  \App\Payment  $payment
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(DestroyPaymentRequest $request, Payment $payment)
-    {
-        $payment->delete();
-        return redirect()->route('rent-payments.index');
     }
 }
