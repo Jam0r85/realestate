57ec6802e362e24560d9654d281dbb803875fd67
= <=>
2017-12-12 12:44:33 +0000

    updated base filter

--- app/ModelFilters/StatementFilter.php
+++ app/ModelFilters/StatementFilter.php
@@ -13,4 +13,18 @@ class StatementFilter extends BaseFilter
     * @var array
     */
     public $relations = [];
+
+    /**
+     * Setup the filter.
+     * 
+     * @return  $this
+     */
+    public function setup()
+    {
+        if (!request('archived')) {
+            $this->whereNull('deleted_at');
+        }
+
+        return $this;
+    }
 }
