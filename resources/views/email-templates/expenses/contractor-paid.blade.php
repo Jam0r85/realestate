ff6aeb0fd79fac448ef9ebfdbbcd9b5e01d4ae58
= <=>
2017-12-13 14:54:41 +0000

    updated expense contractor notification

--- resources/views/email-templates/expenses/contractor-paid.blade.php
+++ resources/views/email-templates/expenses/contractor-paid.blade.php
@@ -1,23 +1,16 @@
 @component('mail::message')
 # Invoice Payment
 
-We have sent you {{ currency($payment->amount) }}
+We have sent you {{ currency($payment->amount) }} by {{ $payment->bank_account ? 'bank transfer' : 'cheque' }}.
 
 @if ($expense->getData('contractor_reference'))
 Your invoice reference is {{ $expense->getData('contractor_reference') }}
 @endif
 
-It relates to the work you carried out at the property -<br />
-<b>{{ $expense->property->present()->fullAddress }}</b>
-
-@if ($payment->bank_account)
-Payment was sent to you by Bank Transfer and normally takes 3 working days.
-@else
-Payment has been sent by Cheque and is in the post.
-@endif
+It relates to the work you carried out at the property - <b>{{ $expense->property->present()->fullAddress }}</b>
 
 @if ($expense->balance <= 0)
-Your invoice {{ currency($expense->cost) }} has been paid in full.
+Your invoice has been paid in full.
 @else
 Your invoice total was {{ currency($expense->cost) }}.
 
