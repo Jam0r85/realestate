13ce2d637af2acc531c0022a360062067a6ad78e
= <=>
2017-12-15 16:19:53 +0000

    updated bank accounts

--- resources/views/bank-accounts/show/payments.blade.php
+++ resources/views/bank-accounts/show/payments.blade.php
@@ -1,1 +1,3 @@
-@include('statement-payments.partials.statement-payments-table', ['payments' => $account->statement_payments])
+@include('statement-payments.partials.statement-payments-table', ['payments' => $account->statement_payments()->with('statement','bank_account')->paginate()])
+
+@include('partials.pagination', ['collection' => $account->statement_payments()->paginate()])
