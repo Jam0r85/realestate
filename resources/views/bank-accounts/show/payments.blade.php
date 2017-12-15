9d1a2dffbcd96590eca8b6f4dd210689fe38a4b1
= <=>
2017-12-15 16:21:59 +0000

    updated bank accounts

--- resources/views/bank-accounts/show/payments.blade.php
+++ resources/views/bank-accounts/show/payments.blade.php
@@ -1,5 +1,5 @@
 @include('statement-payments.partials.statement-payments-table', [
-	'payments' => $account->statement_payments()->with('statement','bank_account','parent')->paginate()
+	'payments' => $account->statement_payments()->with('statement','statement.tenancy','statement.property','bank_account','parent')->paginate()
 ])
 
 @include('partials.pagination', [
