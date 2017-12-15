fc16ed6e0e27f5df6e266014b686d4f114e6be50
= <=>
2017-12-15 14:45:35 +0000

    updated users

--- resources/views/users/show/invoices.blade.php
+++ resources/views/users/show/invoices.blade.php
@@ -1,3 +1,3 @@
-@include('invoices.partials.invoices-table', ['invoices' => $user->invoices()->paginateFilter()])
+@include('invoices.partials.invoices-table', ['invoices' => $user->invoices()->with('users','property','items','items.taxRate','invoiceGroup')->paginateFilter()])
 
 @include('partials.pagination', ['collection' => $user->invoices()->paginateFilter()])
