ca8a29185eb3614bb508fbf23572f71393e5e584
= <=>
2017-12-17 11:31:39 +0000

    updated statements

--- resources/views/statements/partials/statements-table.blade.php
+++ resources/views/statements/partials/statements-table.blade.php
@@ -42,7 +42,20 @@
 					<td>{{ $statement->present()->sendBy(null) }}</td>
 				@endif
 				<td class="text-right text-nowrap">
-					@include('statements.partials.statements-table-action-buttons')
+						
+					@if ($statement->present()->status == 'Paid')
+						<form method="POST" action="{{ route('statements.send', $statement->id) }}" class="d-inline">
+							{{ csrf_field() }}
+							<button type="submit" class="btn btn-info btn-sm">
+								Send
+							</button>
+						</form>
+					@endif
+
+					<a href="{{ route('statements.show', $statement->id) }}" class="btn btn-primary btn-sm">
+						View
+					</a>
+
 				</td>
 			</tr>
 		@endforeach
