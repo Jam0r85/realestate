68a1bf202be82b19e9aa0f32fe588e5cc9be6873
= <=>
2017-12-20 16:48:21 +0000

    updated invoices

--- resources/views/users/show/invoices.blade.php
+++ resources/views/users/show/invoices.blade.php
@@ -1,3 +1,5 @@
-@include('invoices.partials.invoices-table', ['invoices' => $user->invoices()->with('users','property','items','items.taxRate','invoiceGroup')->paginate()])
+@include('invoices.partials.invoices-table', [
+	'invoices' => $invoices = $user->invoices()->with('users','property','items','items.taxRate','invoiceGroup')->paginate()
+])
 
-@include('partials.pagination', ['collection' => $user->invoices()->paginate()])
+@include('partials.pagination', ['collection' => $invoices])
