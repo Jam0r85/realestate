53070fa5a264ec7c03802545c9232b39525d22d9
= <=>
2017-12-20 16:48:46 +0000

    updated bank accounts

--- resources/views/bank-accounts/show/payments.blade.php
+++ resources/views/bank-accounts/show/payments.blade.php
@@ -1,7 +1,7 @@
 @include('statement-payments.partials.statement-payments-table', [
-	'payments' => $account->statement_payments()->with('statement','statement.tenancy','statement.tenancy.property','bank_account','parent')->paginate()
+	'payments' => $payments = $account->statement_payments()->with('statement','statement.tenancy','statement.tenancy.property','bank_account','parent')->paginate()
 ])
 
 @include('partials.pagination', [
-	'collection' => $account->statement_payments()->paginate()
+	'collection' => $payments
 ])
