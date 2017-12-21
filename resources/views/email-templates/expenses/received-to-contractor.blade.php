51f065a8ae865dc61c0fa1ecd69b4100a7680fe0
= <=>
2017-12-21 15:11:28 +0000

    updated expenses

--- resources/views/email-templates/expenses/received-to-contractor.blade.php
+++ resources/views/email-templates/expenses/received-to-contractor.blade.php
@@ -3,6 +3,8 @@
 
 We acknowledge receipt of your invoice {{ $expense->getData('contractor_reference') ? '(' . $expense->getData('contractor_reference') . ')' : '' }} for the property <b>{{ $expense->property->present()->fullAddress }}</b>.
 
+Balance Due: {{ currency($expense->cost) }}
+
 Thanks,<br />
 {{ get_setting('company_name', config('app.name')) }}
 @endcomponent
