bc2cadabe01730e07836c2a4a4f95629a0396fdf
= <=>
2017-12-29 10:27:12 +0000

    updated bank accounts

--- resources/views/bank-accounts/create.blade.php
+++ resources/views/bank-accounts/create.blade.php
@@ -10,29 +10,41 @@
 
 	@endcomponent
 
-	@component('partials.bootstrap.section-with-container')
+	@component('partials.section-with-container')
 
 		@include('partials.errors-block')
 
 		<form method="POST" action="{{ route('bank-accounts.store') }}">
-			{{ csrf_field() }}				
+			{{ csrf_field() }}	
 
-			@include('bank-accounts.partials.form')
+			<div class="card mb-3">
+				@component('partials.card-header')
+					Bank Account Details
+				@endcomponent
+				<div class="card-body">			
 
-			<div class="form-group">
-				<label for="users">Users</label>
-				<select name="users[]" id="users" class="form-control select2" multiple>
-					@foreach (users() as $user)
-						<option value="{{ $user->id }}">
-							{{ $user->present()->selectName }}
-						</option>
-					@endforeach
-				</select>
+					@include('bank-accounts.partials.form')
+
+					<div class="form-group">
+						<label for="users">Users</label>
+						<select name="users[]" id="users" class="form-control select2" multiple>
+							@foreach (users() as $user)
+								<option value="{{ $user->id }}">
+									{{ $user->present()->selectName }}
+								</option>
+							@endforeach
+						</select>
+					</div>
+
+				</div>
+				<div class="card-footer">
+
+					@component('partials.save-button')
+						Create Bank Account
+					@endcomponent
+
+				</div>
 			</div>
-
-			@component('partials.save-button')
-				Create Bank Account
-			@endcomponent
 
 		</form>
 
