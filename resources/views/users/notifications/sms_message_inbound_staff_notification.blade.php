1892c4b7051d3e6256cac9cf0039a6ab5e7c233c
= <=>
2017-12-30 10:53:22 +0000

    added default notification template

--- resources/views/users/notifications/sms_message_inbound_staff_notification.blade.php
+++ resources/views/users/notifications/sms_message_inbound_staff_notification.blade.php
@@ -1,18 +1,16 @@
-@component('partials.card-header')
-	SMS Received
+@component('users.notifications.template')
+	@slot('header')
+		SMS Received
+	@endslot
+
+	<div class="card-body">
+		<blockquote class="blockquote">
+			<p class="mb-0">
+				{{ $notification->data['text'] }}
+			</p>
+			<footer class="blockquote-footer">
+				SMS message sent by <a href="{{ route('users.show', $notification->notifiable->user_id) }}">{{ $notification->data['user'] }}</a>
+			</footer>
+		</blockquote>
+	</div>
 @endcomponent
-
-<div class="card-body">
-	<blockquote class="blockquote">
-		<p class="mb-0">
-			{{ $notification->data['text'] }}
-		</p>
-		<footer class="blockquote-footer">
-			SMS message sent by <a href="{{ route('users.show', $notification->notifiable->user_id) }}">{{ $notification->data['user'] }}</a>
-		</footer>
-	</blockquote>
-</div>
-
-<div class="card-footer">
-	{{ datetime_formatted($notification->created_at) }}
-</div>
--- resources/views/users/notifications/sms_owner_delivery_receipt_notification.blade.php
+++ resources/views/users/notifications/sms_owner_delivery_receipt_notification.blade.php
@@ -1,18 +1,16 @@
-@component('partials.card-header')
-	SMS Delivery Receipt
+@component('users.notifications.template')
+	@slot('header')
+		SMS Delivery Receipt
+	@endslot
+
+	<div class="card-body">
+		<blockquote class="blockquote">
+			<p class="mb-0">
+				{{ $notification->data['text'] }}
+			</p>
+			<footer class="blockquote-footer">
+				SMS message to {{ $notification->data['recipient'] }} was {{ $notification->data['status'] }}
+			</footer>
+		</blockquote>
+	</div>
 @endcomponent
-
-<div class="card-body">
-	<blockquote class="blockquote">
-		<p class="mb-0">
-			{{ $notification->data['text'] }}
-		</p>
-		<footer class="blockquote-footer">
-			SMS message to {{ $notification->data['recipient'] }} was {{ $notification->data['status'] }}
-		</footer>
-	</blockquote>
-</div>
-
-<div class="card-footer">
-	{{ datetime_formatted($notification->created_at) }}
-</div>
