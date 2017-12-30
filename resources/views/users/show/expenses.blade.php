74f0c9df5061f5487d9921f9c52ecfab126a0ed6
= <=>
2017-12-30 09:39:55 +0000

    added new info alert to user expenses as well as adding documents to the eager loading

--- resources/views/users/show/expenses.blade.php
+++ resources/views/users/show/expenses.blade.php
@@ -1,5 +1,11 @@
+@component('partials.alerts.info')
+	This user has been assigned as contractor for the expenses below.
+@endcomponent
+
 @include('expenses.partials.expenses-table', [
-	'expenses' => $expenses = $user->expenses()->with('property','contractor')->paginate()
+	'expenses' => $expenses = $user->expenses()->with('property','contractor','documents')->paginate()
 ])
 
-@include('partials.pagination', ['collection' => $expenses])
+@include('partials.pagination', [
+	'collection' => $expenses
+])
