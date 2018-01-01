31c58ebfed6306b2fe8b2fa497902d30a093d1dd
= <=>
2018-01-01 12:26:22 +0000

    added new storage driver using Digital Ocean

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,15 @@ return [
             'visibility' => 'public',
         ],
 
+        'storage' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_STORAGE_BUCKET'),
+        ],
+
         'backup' => [
             'driver' => 's3',
             'key' => env('DO_SPACES_KEY'),
