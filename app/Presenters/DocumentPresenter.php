beba0366a535f5f3210cd0702411237c952d9add
= <=>
2018-01-01 21:20:44 +0000

    added parent name and badge to expenses tab;e

--- app/Presenters/DocumentPresenter.php
+++ app/Presenters/DocumentPresenter.php
@@ -7,6 +7,28 @@ class DocumentPresenter extends BasePresenter
 	/**
 	 * @return string
 	 */
+	public function parentName()
+	{
+		if (model_name($this->parent) == 'Expense') {
+			return $this->parent->name;
+		}
+
+		if (model_name($this->parent) == 'Tenancy') {
+			return $this->parent->present()->name;
+		}
+	}
+
+	/**
+	 * @return string
+	 */
+	public function parentBadge()
+	{
+		return model_name($this->parent);
+	}
+
+	/**
+	 * @return string
+	 */
 	public function status()
 	{
 		if ($this->deleted_at) {
---
+++ resources/views/documents/partials/table.blade.php
@@ -0,0 +1,29 @@
+@component('partials.table')
+	@slot('header')
+		<th>Status</th>
+		<th>Uploaded</th>
+		<th>Name</th>
+		<th>Parent</th>
+		<th></th>
+	@endslot
+	@slot('body')
+		@foreach ($documents as $document)
+			<tr>
+				<td>{{ $document->present()->status }}</td>
+				<td>{{ date_formatted($document->created_at) }}</td>
+				<td>{{ $document->name }}</td>
+				<td>
+					<span class="badge badge-secondary">
+						{{ $document->present()->parentBadge }}
+					</span>
+					{{ $document->present()->parentName }}
+				</td>
+				<td class="text-right">
+					<a href="{{ route('documents.edit', $document->id) }}" class="btn btn-warning btn-sm">
+						@icon('edit')
+					</a>
+				</td>
+			</tr>
+		@endforeach
+	@endslot
+@endcomponent
