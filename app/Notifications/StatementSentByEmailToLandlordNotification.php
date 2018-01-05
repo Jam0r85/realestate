b3ae4fee4484f0ac3a539b40732531088ffff57c
= <=>
2018-01-05 16:34:52 +0000

    missed the dependencies, added them

--- app/Notifications/StatementSentByEmailToLandlordNotification.php
+++ app/Notifications/StatementSentByEmailToLandlordNotification.php
@@ -2,10 +2,12 @@
 
 namespace App\Notifications;
 
+use App\Statement;
 use Illuminate\Bus\Queueable;
-use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Support\Facades\Storage;
 
 class StatementSentByEmailToLandlordNotification extends Notification
 {
