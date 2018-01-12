569bbeb3b08f11d5100cbfc6c64c0adcf6d60e7e
= <=>
2018-01-12 12:11:32 +0000

    added pounds to pence converter

---
+++ app/Console/Commands/FormatPenceToPounds.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Payment;
+use Illuminate\Console\Command;
+
+class FormatPenceToPounds extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'format:pence-to-pounds';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        Payment::chunk(200, function ($payments) {
+            foreach ($payments as $payment) {
+                $payment->update(['amount' => $payment->amount / 100]);
+            }
+        });
+
+        $this->info('Updated Payments');
+    }
+}
