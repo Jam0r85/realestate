efdab04625087cb6b9c530ea32c47d91ca01509d
= <=>
2018-01-12 18:30:22 +0000

    updated receipts

--- resources/views/pdf/payment.blade.php
+++ resources/views/pdf/payment.blade.php
@@ -37,7 +37,7 @@
 				
 				<p>Method - {{ $payment->method->name }}</p>
 				<p>Recorded By - {{ $payment->owner->present()->fullName }}</p>
-				<p><b>Amount - {{ currency($payment->amount) }}</b></p>
+				<p><b>Amount - {{ money_formatted($payment->amount) }}</b></p>
 			</div>
 
 			<hr />
