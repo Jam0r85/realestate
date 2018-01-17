56b1ccbb2b642b6a3d2462e548b6124c910a5dbc
= <=>
2018-01-17 12:00:45 +0000

    updated base presenter

--- resources/views/pdf/statement.blade.php
+++ resources/views/pdf/statement.blade.php
@@ -91,9 +91,9 @@
 										<small>(Part Payment)</small>
 									@endif
 								</td>
-								<td>{{ $expense->present()->money('pivot.amount') }}</td>
+								<td>{{ $expense->present()->money('amount') }}</td>
 								<td></td>
-								<td>{{ $expense->present()->money('pivot.amount') }}</td>
+								<td>{{ $expense->present()->money('amount') }}</td>
 							</tr>
 						@endforeach
 
