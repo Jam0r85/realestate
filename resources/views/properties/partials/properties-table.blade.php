dd84bc759580f1eaec58641283ebb947e5e169e0
= <=>
2018-01-17 16:49:49 +0000

    updated properties table

--- resources/views/properties/partials/properties-table.blade.php
+++ resources/views/properties/partials/properties-table.blade.php
@@ -2,18 +2,12 @@
 	@slot('header')
 		<th>Name</th>
 		<th class="text-right">Owners</th>
-		<th></th>
 	@endslot
 	@slot('body')
 		@foreach ($properties as $property)
-			<tr>
+			<tr class="clickable-row" data-href="{{ route('properties.show', $property->id) }}" data-toggle="tooltip" data-placement="left" title="View {{ $property->present()->shortAddress }}">
 				<td>{{ $property->present()->fullAddress }}</td>
 				<td class="text-right">{!! $property->present()->ownerBadges !!}</td>
-				<td class="text-right">
-					<a href="{{ route('properties.show', $property->id) }}" class="btn btn-primary btn-sm">
-						@icon('view')
-					</a>
-				</td>
 			</tr>
 		@endforeach
 	@endslot
