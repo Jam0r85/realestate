3150f9b8b64cccc43261ca02e1c08da8a642e4fa
= <=>
2018-01-18 14:16:13 +0000

    updated permissions table

--- resources/views/permissions/partials/table.blade.php
+++ resources/views/permissions/partials/table.blade.php
@@ -1,15 +1,15 @@
 @component('partials.table')
 	@slot('header')
 		<th>Name</th>
-		<th>Slug</th>
 		<th>Description</th>
+		<th class="text-right">Slug</th>
 	@endslot
 	@slot('body')
 		@foreach ($permissions as $permission)
 			<tr class="clickable-row" data-href="{{ route('permissions.edit', $permission->id) }}" data-toggle="tooltop" data-placement="left" title="Edit Permission">
 				<td>{{ $permission->name }}</td>
-				<td>{{ $permission->slug }}</td>
 				<td>{{ $permission->description }}</td>
+				<td class="text-right">{{ $permission->slug }}</td>
 			</tr>
 		@endforeach
 	@endslot
