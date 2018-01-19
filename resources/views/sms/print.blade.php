31a6fb02dd90df7eb69352f789426b58e46848f5
= <=>
2018-01-19 11:08:52 +0000

    added title and date to sms print message

--- resources/views/sms/print.blade.php
+++ resources/views/sms/print.blade.php
@@ -3,6 +3,12 @@
 @section('content')
 
 	@component('partials.section-with-container')
+
+		<h2>
+			SMS Message
+			<small class="text-muted">{{ \Carbon\Carbon::now()->toDayDateTimeString() }}</small>
+		</h2>
+
 		@include('sms.partials.message')
 	@endcomponent
 
