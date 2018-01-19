85c7778b650c7e2fb94dd888db408e748cee8eb5
= <=>
2018-01-19 10:45:41 +0000

    updated SMS history layout

--- resources/views/sms/partials/message.blade.php
+++ resources/views/sms/partials/message.blade.php
@@ -1,26 +1,26 @@
-<div class="card mb-3 {{ $message->isInbound() ? 'border' : 'text-white bg' }}-{{ $message->status('class') }}">
-
-	@component('partials.card-header')
-		{{ $message->phone_number }}
-
-		@if ($message->recipient)
-			- 
-			<a href="{{ route('users.show', $message->recipient_id) }}">
+@component('partials.card')
+	@slot('card-header')
+		@if ($message->isInbound())
+			@icon('received') <b>received</b> from {{ $message->phone_number }}
+			@if ($message->recipient)
+				{{ $message->recipient()->present()->fullName }}
+			@endif
+		@else
+			@icon('sent') <b>sent</b> to {{ $message->phone_number }}
+			<span class="badge badge-secondary">
 				{{ $message->recipient->present()->fullName }}
-			</a>
+			</span>
 		@endif
-
-		@slot('small')
-			{{ $message->present()->timeSince('created_at') }}
-		@endslot
-	@endcomponent
-
-	<div class="card-body">
-
-		<p class="card-text">
-			{{ $message->body }}
-		</p>
-		
-	</div>
-
-</div>
+	@endslot
+	@slot('body')
+		<div class="flex-column align-items-start">
+			<small>
+				{{ $message->present()->timeSince('created_at') }}
+			</small>
+			<p class="card-text mb-1">
+				{{ $message->body }}
+			</p>
+			<small>{{ $message->status() }}</small>
+		</div>
+	@endslot
+@endcomponent
