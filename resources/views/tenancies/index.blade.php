b2296cb13d09e9222bdd01e64ac55e9a1213024c
= <=>
2018-01-24 12:27:40 +0000

    updated tenancies index to show info alert when searching

--- resources/views/tenancies/index.blade.php
+++ resources/views/tenancies/index.blade.php
@@ -20,70 +20,76 @@
 
 		@include('partials.index-search', ['route' => 'tenancies.search', 'session' => 'tenancy_search_term'])
 
-		<ul class="nav nav-pills">
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					{{ request('service') ? slug_to_name(request('service')) : 'Services' }}
-				</a>
-				<div class="dropdown-menu">
-					<a class="dropdown-item @if (!request('service')) active @endif" href="{{ Filter::link(['service' => null]) }}">
-						All Services
+		@if (!session()->has('invoice_search_term'))
+			<ul class="nav nav-pills">
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						{{ request('service') ? slug_to_name(request('service')) : 'Services' }}
 					</a>
-					<div class="dropdown-divider"></div>
-					@foreach (common('services') as $service)
-						<a class="dropdown-item @if (request('service') == $service->slug) active @endif" href="{{ Filter::link(['service' => $service->slug]) }}">
-							{{ $service->name }}
+					<div class="dropdown-menu">
+						<a class="dropdown-item @if (!request('service')) active @endif" href="{{ Filter::link(['service' => null]) }}">
+							All Services
 						</a>
-					@endforeach
-				</div>
-			</li>
+						<div class="dropdown-divider"></div>
+						@foreach (common('services') as $service)
+							<a class="dropdown-item @if (request('service') == $service->slug) active @endif" href="{{ Filter::link(['service' => $service->slug]) }}">
+								{{ $service->name }}
+							</a>
+						@endforeach
+					</div>
+				</li>
 
-			{!! (new Filter())->monthDropdown() !!}
-			{!! (new Filter())->yearDropdown('App\Tenancy', 'started_on') !!}
+				{!! (new Filter())->monthDropdown() !!}
+				{!! (new Filter())->yearDropdown('App\Tenancy', 'started_on') !!}
 
-			<li class="nav-item dropdown">
-				<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-					{{ request('status') ? slug_to_name(request('status')) : 'Status' }}
-				</a>
-				<div class="dropdown-menu">
-					<a class="dropdown-item @if (!request('status')) active @endif" href="{{ Filter::link(['status' => null]) }}">
-						All Statuses
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						{{ request('status') ? slug_to_name(request('status')) : 'Status' }}
 					</a>
-					<div class="dropdown-divider"></div>
-					<a class="dropdown-item @if (request('status') == 'starting') active @endif" href="{{ Filter::link(['status' => 'starting']) }}">
-						Starting
+					<div class="dropdown-menu">
+						<a class="dropdown-item @if (!request('status')) active @endif" href="{{ Filter::link(['status' => null]) }}">
+							All Statuses
+						</a>
+						<div class="dropdown-divider"></div>
+						<a class="dropdown-item @if (request('status') == 'starting') active @endif" href="{{ Filter::link(['status' => 'starting']) }}">
+							Starting
+						</a>
+						<a class="dropdown-item @if (request('status') == 'vacating') active @endif" href="{{ Filter::link(['status' => 'vacating']) }}">
+							Vacating
+						</a>
+						<a class="dropdown-item @if (request('status') == 'vacated') active @endif" href="{{ Filter::link(['status' => 'vacated']) }}">
+							Vacated
+						</a>
+					</div>
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link @if (request('overdue')) active @endif" href="{{ request('overdue') ? Filter::link(['overdue' => null]) : Filter::link(['overdue' => true]) }}">
+						Overdue
 					</a>
-					<a class="dropdown-item @if (request('status') == 'vacating') active @endif" href="{{ Filter::link(['status' => 'vacating']) }}">
-						Vacating
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link @if (request('has_rent_balance')) active @endif" href="{{ request('has_rent_balance') ? Filter::link(['has_rent_balance' => null]) : Filter::link(['has_rent_balance' => true]) }}">
+						Has Rent
 					</a>
-					<a class="dropdown-item @if (request('status') == 'vacated') active @endif" href="{{ Filter::link(['status' => 'vacated']) }}">
-						Vacated
+				</li>
+
+				<li class="nav-item">
+					<a class="nav-link @if (request('owes_rent')) active @endif" href="{{ request('owes_rent') ? Filter::link(['owes_rent' => null]) : Filter::link(['owes_rent' => true]) }}">
+						Owes Rent
 					</a>
-				</div>
-			</li>
+				</li>
 
-			<li class="nav-item">
-				<a class="nav-link @if (request('overdue')) active @endif" href="{{ request('overdue') ? Filter::link(['overdue' => null]) : Filter::link(['overdue' => true]) }}">
-					Overdue
-				</a>
-			</li>
+				{!! Filter::archivePill() !!}
+				{!! Filter::clearButton() !!}
 
-			<li class="nav-item">
-				<a class="nav-link @if (request('has_rent_balance')) active @endif" href="{{ request('has_rent_balance') ? Filter::link(['has_rent_balance' => null]) : Filter::link(['has_rent_balance' => true]) }}">
-					Has Rent
-				</a>
-			</li>
-
-			<li class="nav-item">
-				<a class="nav-link @if (request('owes_rent')) active @endif" href="{{ request('owes_rent') ? Filter::link(['owes_rent' => null]) : Filter::link(['owes_rent' => true]) }}">
-					Owes Rent
-				</a>
-			</li>
-
-			{!! Filter::archivePill() !!}
-			{!! Filter::clearButton() !!}
-
-		</ul>
+			</ul>
+		@else
+			@component('partials.alerts.info')
+				@icon('info') Search does not include archived/deleted tenancies.
+			@endcomponent
+		@endif
 
 		@include('tenancies.partials.tenancies-table')
 		@include('partials.pagination', ['collection' => $tenancies])
