c5809c107d7b96fa38c390c13614dfa69542984d
= <=>
2018-01-29 09:18:57 +0000

    updated PDF statement discount name

--- resources/views/pdf/statement.blade.php
+++ resources/views/pdf/statement.blade.php
@@ -68,7 +68,7 @@
 											@if (strpos(strtolower($item->description), 'service') && $statement->tenancy->serviceDiscounts)
 												<br />
 												@foreach ($statement->tenancy->serviceDiscounts as $discount)
-													<small>Discount - {{ $discount->present()->nameWithAmount }}</small> <br />
+													<small>Including {{ $discount->present()->nameWithAmount }}</small> <br />
 												@endforeach
 											@endif
 										</td>
