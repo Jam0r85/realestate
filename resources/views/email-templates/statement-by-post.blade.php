6786c2276cf65047c0b5b51e5cf1273a4a4170fc
= <=>
2018-02-02 10:46:48 +0000

    updated rental statement by post email template

--- resources/views/email-templates/statement-by-post.blade.php
+++ resources/views/email-templates/statement-by-post.blade.php
@@ -1,13 +1,9 @@
 @component('mail::message')
 # Rental Statement
 
-We have processed the rental statement for <b>{{ $statement->property()->present()->fullAddress }}</b>.
+We have processed the new rental statement for <b>{{ $statement->property()->present()->fullAddress }}</b>.
 
 The statement has been posted to you.
-
-@component('mail::panel')
-Would you prefer to have the renal statement attached to this e-mail? Let us know by simply replying to this e-mail.
-@endcomponent
 
 @include('email-templates.partials.footer')
 
