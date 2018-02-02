548d064dd13c9cfa7d7e66f65c7a5e337fb184ab
= <=>
2018-02-02 11:38:05 +0000

    updated user statement notifications

--- resources/views/users/notifications/statement_sent_by_email_to_landlord_notification.blade.php
+++ resources/views/users/notifications/statement_sent_by_email_to_landlord_notification.blade.php
@@ -3,6 +3,8 @@
 		{{ datetime_formatted($notification->created_at) }}
 	@endslot
 	@slot('body')
-		Statement {{ $notification->data['statement_id'] }} was sent by E-Mail to the Landlord.
+		<a href="{{ route('statements.show', $notification->data['statement_id']) }}">
+			Statement <b>{{ $notification->data['statement_id'] }}</b>
+		</a> was sent by e-mail to the landlord.
 	@endslot
 @endcomponent
